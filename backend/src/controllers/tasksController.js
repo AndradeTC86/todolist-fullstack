ea874187361b2a71585cb839e685ccb1a9765b08
Thiago Cesar <andrade-tc@uol.com.br>
2024-10-20 11:09:23 -0300

    Alterando o projeto

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,12 +1,16 @@
 const tasksModel = require('../models/tasksModel')
 
-const getAll = async (request, response) => {
-
+const getAll = async (_request, response) => {
     const tasks = await tasksModel.getAll()
     return response.status(200).json(tasks)
+}
 
+const createTask = async (request, response) => {
+    //const createdTask = await tasksModel.createTask()
+    return response.status(201).json(request.body)
 }
 
 module.exports = {
-    getAll
+    getAll,
+    createTask
 }
